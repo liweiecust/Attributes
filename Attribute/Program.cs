2f639fa0904644b7540e6e8edb5b7198e6e1b9c7
liwei <liweiecust@163.com>
2019-11-01 18:27:07 +0800

    up

---
+++ Attribute/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attribute
+{
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Type tp = typeof(MyClass);
+            Type tb = typeof(VersionAttribute);
+            VersionAttribute version = (VersionAttribute)System.Attribute.GetCustomAttribute(tp, typeof(VersionAttribute));
+            Console.WriteLine("Name=" + version.Name);
+            Console.WriteLine("Date=" + version.Date);
+            Console.WriteLine("Description=" + version.Description);
+            version.Show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Attribute/VersionAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class VersionAttribute : System.Attribute
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+
+        public string DD { get; set; }
+
+
+        public void Show()
+        {
+            Console.WriteLine("名称：{0} 日期： {1} 描述：{2}", Name, Date, Description);
+        }
+    }
+
+    [Version(Name ="版本检测", Date ="2016-9-18", Description="刚会用来一发")]
+    public class MyClass
+    {
+    }
+}
